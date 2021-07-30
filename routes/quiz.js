f3e794a13d3d1c713abd4d7d9e04c339798c4485
winters0727 <winters0727@gmail.com>
2021-07-30 22:52:38 +0900

    feature / add easter egg

--- routes/quiz.js
+++ routes/quiz.js
@@ -49,4 +49,30 @@ router.get('/random', async (req, res, next) => {
     
 })
 
+router.get('/boostcamp', async(req, res, next) => {
+    await res.status(200).json({
+        "Project(40) Contributor" : "릴레이 프로젝트(40) 기여자들",
+        "1주차" : [
+            "J086_박상진",
+            "J126_오수완",
+            "J146_이영범",
+            "J151_이원주",
+            "J166_이충헌",
+            "K044_이원중",
+            "S016_박상우",
+            "S043_이정엽"
+        ],
+        "2주차" : [
+            "J052_김정윤",
+            "J056_김준호",
+            "J061_김창현",
+            "J091_박주원",
+            "J094_박춘화",
+            "K041_이상건",
+            "K047_이효동",
+            "S037_이남준"
+        ]
+    })
+})
+
 module.exports = router;
