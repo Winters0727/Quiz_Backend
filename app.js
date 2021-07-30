af2d4b67314051784da741ddd92a7667fa54406f
winters0727 <winters0727@gmail.com>
2021-07-30 23:34:08 +0900

    feature / add cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors')
 
 const quizRouter = require('./routes/quiz')
 
@@ -16,6 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());  
+app.use(cors())
 
 app.use('/api/quiz', quizRouter)
 
